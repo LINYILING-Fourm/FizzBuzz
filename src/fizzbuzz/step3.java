50888c89a74c383577b6d18516088b5ac0244e1a
LINYILING-Fourm <y.lin@fourm.com>
2024-06-17 15:20:25 +0900

    step3

--- src/fizzbuzz/step3.java
+++ src/fizzbuzz/step3.java
@@ -1,6 +1,6 @@
 package fizzbuzz;
 
-public class step1 {
+public class step3 {
 
 	public class Main {
 	    public static void main(String[] args) {
@@ -8,9 +8,13 @@ public class step1 {
 	    	
 	        
 	        for (int x = 1; x<31;x++){
-	        	if(x%3==0) {
+	        	if (x % 3 == 0 && x % 5 == 0) {
+	                System.out.println("Fizz,Buzz");
+	        	}else if(x%3==0) {
 	        		System.out.println("fizz ");
-	        	}else {
+	        	}else if(x%5==0) {
+	        		System.out.println("buzz ");
+	        	}else{
 	        		 System.out.println(x );
 	        	}
 	        	
