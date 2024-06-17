2866808ecb6018bfae4f29363cb182bdb904b63e
LINYILING-Fourm <y.lin@fourm.com>
2024-06-17 15:41:41 +0900

    step5

--- src/fizzbuzz/step3.java
+++ src/fizzbuzz/step3.java
@@ -8,32 +8,25 @@ public class step3 {
 	    pr();
 	    
 	}
-	    
-	        
-	    	
-	public  static void pr() {    
-		
-	        int y = 0;
-	        for (int x = 1; x<31;x++){
 	        	
-	        
+	public  static void pr() {    		
+	        int y = 0;
+	        for (int x = 30; x>0;x--){              
 	        	if (x % 3 == 0 && x % 5 == 0) {
-	                System.out.println("Fizz,Buzz");
+	                System.out.println("FizzBuzz");
 	        	}else if(x%3==0) {
-	        		System.out.println("fizz ");
+	        		System.out.println("fizz");
 	        	}else if(x%5==0) {
-	        		System.out.println("buzz ");
+	        		System.out.println("buzz");
 	        	}else{
 	        		if(y!=0) {
-	       
 	        		 System.out.println(x+"("+y+")" );}
 	        		else {
 	        			System.out.println(x);
 	        		}
 	        		 y=x;
 	        		}
-	        	}
-	        	
+	        	}	        	
 	        }       
 	               
 	    }	
