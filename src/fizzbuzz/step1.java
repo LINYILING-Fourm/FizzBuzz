baecd4c5f18033b60d905149caf2e6ee1eb405d3
LINYILING-Fourm <y.lin@fourm.com>
2024-06-17 15:11:59 +0900

    你的提交消息

--- src/fizzbuzz/step1.java
+++ src/fizzbuzz/step1.java
@@ -4,70 +4,20 @@ public class step1 {
 
 	public class Main {
 	    public static void main(String[] args) {
-	        int a = 1;
-	        int b = 2;
-	        int c = 3;
-	        int d = 4;
-	        int e = 5;
-	        int f = 6;
-	        int g = 7;
-	        int h = 8;
-	        int i = 9;
-	        int j = 10;
-	        int k = 11;
-	        int l = 12;
-	        int m = 13;
-	        int n = 14;
-	        int o = 15;
-	        int p = 16;
-	        int q = 17;
-	        int r = 18;
-	        int s = 19;
-	        int t = 20;
-	        int u = 21;
-	        int v = 22;
-	        int w = 23;
-	        int x = 24;
-	        int y = 25;
-	        int z = 26；
-	        int aa = 27;
-	        int bb = 28;
-	        int cc = 29;
-	        int dd = 30;
-
-	        // Print all variables
-	        System.out.println("a = " + a);
-	        System.out.println("b = " + b);
-	        System.out.println("c = " + c);
-	        System.out.println("d = " + d);
-	        System.out.println("e = " + e);
-	        System.out.println("f = " + f);
-	        System.out.println("g = " + g);
-	        System.out.println("h = " + h);
-	        System.out.println("i = " + i);
-	        System.out.println("j = " + j);
-	        System.out.println("k = " + k);
-	        System.out.println("l = " + l);
-	        System.out.println("m = " + m);
-	        System.out.println("n = " + n);
-	        System.out.println("o = " + o);
-	        System.out.println("p = " + p);
-	        System.out.println("q = " + q);
-	        System.out.println("r = " + r);
-	        System.out.println("s = " + s);
-	        System.out.println("t = " + t);
-	        System.out.println("u = " + u);
-	        System.out.println("v = " + v);
-	        System.out.println("w = " + w);
-	        System.out.println("x = " + x);
-	        System.out.println("y = " + y);
-	        System.out.println("z = " + z);
-	        System.out.println("aa = " + aa);
-	        System.out.println("bb = " + bb);
-	        System.out.println("cc = " + cc);
-	        System.out.println("dd = " + dd);
+	        
+	    	
+	        
+	        for (int x = 1; x<31;x++){
+	        	if(x%3==0) {
+	        		System.out.println("fizz ");
+	        	}else {
+	        		 System.out.println(x );
+	        	}
+	        	
+	        }       
+	               
 	    }
+	     
 	}
-
-
+	
 }
